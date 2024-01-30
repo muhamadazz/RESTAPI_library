0d80a19381f41537e3eecf4a42ec84f5366bc124
Muhammad Fahraz Firdaus <fahrazfirdaus@gmail.com>
2024-01-30 17:34:29 +0700

    berhasil menambahkan 2 endpoint : show all book dan show book by id

--- controller.js
+++ controller.js
@@ -6,3 +6,27 @@ var connection = require('./config/connection');
 exports.index = function(req,res){
     response.ok("running my Rest API", res)
 };
+
+//1. Get a list of all books
+exports.allbooks = function(req,res) {
+    connection.query('SELECT * FROM book', function(error, rows, fields){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok(rows, res);
+        }
+    });
+};
+
+//2. Get details of a book by book id
+exports.ShowBookbyId = function(req, res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM book WHERE book_id = ?', [id],
+        function(error, rows, fields){
+            if(error){
+                console.log(error);
+            }else{
+                response.ok(rows, res);
+            }
+        });
+};
--- routes.js
+++ routes.js
@@ -4,5 +4,10 @@ module.exports = function(app) {
     var myjson = require('./controller')
 
     app.route('/')
-        .get(myjson.index)
+        .get(myjson.index);
+    
+    app.route('/book')
+        .get(myjson.allbooks);
+    app.route('/book/:id')
+        .get(myjson.ShowBookbyId);
 }
