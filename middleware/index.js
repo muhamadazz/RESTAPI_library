1c81dd0a304fd866bb04108e715a1808778aeef3
Muhammad Fahraz Firdaus <fahrazfirdaus@gmail.com>
2024-02-02 15:09:10 +0700

    berhasil membuat jwt verification

--- middleware/index.js
+++ middleware/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var auth = require('./auth');
 var router = express.Router();
+var verification = require('./verification');
 
 //daftarkan menu registrasi
 router.post('/api/v1/register', auth.registration);
-router.post('/api/v1/login', auth.login);
+router.post('/api/v1/login', auth.login); 
+
+// alamat yang perlu otorisasi
+router.get('/api/v1/secret', verification(2), auth.secretPage);
 
 module.exports = router;
